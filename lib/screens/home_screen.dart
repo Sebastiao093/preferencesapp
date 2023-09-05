c71d5a8c3ed1379256b201976d59b5092bfa6ae7
Sebastián H Giraldo <47760039+Sebastiao093@users.noreply.github.com>
2023-09-05 17:45:39 -0500

    Add files via upload

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:preferences_app/share_preferences/preferences.dart';
+import 'package:preferences_app/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static String routerName = 'Home';
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      drawer: SideMenu(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Darkmode: ${ Preferences.isDarkmode }'),
+          Divider(),
+          Text('Genero: ${ Preferences.gender }'),
+          Divider(),
+          Text('Nombre de usuario: ${ Preferences.name }'),
+          Divider(),
+        ],
+      )
+    );
+  }
+}
